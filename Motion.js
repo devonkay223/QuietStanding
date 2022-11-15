ba3bfd922fca77b790a9aa54522ca827fc8bbc98
devonkay223 <devonkay223@gmail.com>
2022-11-14 23:12:19 -0800

    motion js

--- Motion.js
+++ Motion.js
@@ -1,3 +1,18 @@
+const devicemotion = new DeviceMotionEvent("devicemotion")
+
+if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', deviceMotionHandler);
+    // setTimeout(stopJump, 3 * 1000);
+    console.log('supported');
+  }
+else {
+    console.log("not supported");
+}
+
 window.addEventListener('devicemotion', (event) => {
     console.log(`${event.acceleration.x} m/s2`);
   });
+
+function deviceMotionHandler(){
+   // do stuff here
+}
